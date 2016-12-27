ff1a864c6cd6bd6bc977149bc7698fe5b8e5375f
sebeweb <sebbeweb@gmail.com>
2016-12-27 20:19:14 +0100

    debut du triangle de Pascal

--- exo1/exo1.java
+++
@@ -1,8 +0,0 @@
-public class exo1
-{
-	public static void main(String[] args)
-	{
-		String hello = "hello world";
-		System.out.println(hello);
-	}
-}
--- exo1/exo1.php
+++
@@ -1,4 +0,0 @@
-<?php
-$hello = "hello world";
-
-echo "$hello\n";
