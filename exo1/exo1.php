7a06b77ede244fd0f9ac352e3154d00f1ef216c6
sebeweb <sebbeweb@gmail.fr>
2016-12-28 12:35:21 +0100

    exo 2 terminer

---
+++ exo1/exo1.php
@@ -0,0 +1,9 @@
+<?php
+//Test de la sortie standard
+//by sebastien
+//beweb
+
+$salut = "hello world";
+echo $salut."\r\n";
+
+?>
---
+++ exo1/exo1.py
@@ -0,0 +1,7 @@
+#Test de la sortie standard en python
+#by sebastien
+#beweb
+
+
+salut = "hello world"
+print salut
